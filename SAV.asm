fc75bf0146dc10ed44bacd0f9dc2755de5a70535
Franaffel <151704994+FraN-onLine@users.noreply.github.com>
2025-04-24 14:58:06 +0800

    yori mo a-na-ta

--- SAV.asm
+++ SAV.asm
@@ -63,13 +63,40 @@ islastdigit:
     la $t1, ints #integers
     li $t3, 0 # index being compared
     
-bubblesort:
     
-    bubbleloop1:
-    	lbu	$t2, 0($t1) #load no 1 to compare
+bubblesort:
+	li $t5, 0 #counter 2
+	 la $t1, ints #integers from the start once more
+	 
+	 #need base case to check if soeted here-
+	
+bubbleloopmain: #will print out all til we reach the index of comparison
+   
+   beq $t5, $t3, bubbleloop1
+    lw $t2, 0($t1) #load on t2
+    addi $t5, $t5, 1 #counter 2++
+    add $t1, $t1, 4 #shift 4 for each int
+    
+    li $v0, 1
+    move $a0, $t2
+    syscall
+    
+     li $v0, 4
+    la $a0, space
+    syscall
+    
+    blt $t5, 6, bubbleloopmain
+    addi $t3, $t3, 1 #index being compared++
+    li $v0, 11
+    li $a0, '\n'
+    syscall
+    j bubblesort
+    
+    bubbleloop1: #this occurs when we reach the indices to be compared
+    	lw	$t2, 0($t1) #load no 1 to compare
         addi $t0, $t0, 1 #i++
         add $t1, $t1, 4 #shift 4 for each int
-        lbu	$t4, 0($t1) #load number 2 to compare
+        lw	$t4, 0($t1) #load number 2 to compare
     
         li $v0, 11
         li $a0, '['
@@ -94,6 +121,10 @@ bubblesort:
     	li $v0, 11
     	li $a0, ']'
    	 syscall
+   	 
+   	  li $v0, 4
+         la $a0, space
+        syscall
     	
     	bgt	$t2, $t4, swap
     	j skipswap
@@ -103,9 +134,13 @@ bubblesort:
     		sw $t4 -4($t1)
     	
     	skipswap:
-        li $v0, 11
-    	li $a0, '\n'
-   	 syscall
-
-   	 blt $t0, 5, bubbleloop1
+    	add $t1, $t1, 4 #shift 4 for each int
+	
+	addi $t5, $t5 2 #counter 2 += 2
+	blt  $t5, 6, bubbleloopmain
+	li $t3, 0 #index being compared++
+	 li $v0, 11
+        li $a0, '\n'
+        syscall
+   	 j bubblesort
     	
