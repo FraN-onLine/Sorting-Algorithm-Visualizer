a1008554233b2043630b6389b0d2282b8040e70d
Franaffel <151704994+FraN-onLine@users.noreply.github.com>
2025-04-24 13:17:12 +0800

    Update SAV.asm

--- SAV.asm
+++ SAV.asm
@@ -1,1 +1,58 @@
-#There lived a certain man in russia long ago
+.data
+ints: .space 24
+prompt: .asciiz "Enter a number: "
+originalarray: .asciiz "Original Array is: "
+res: .asciiz "result: "
+com_space: .asciiz ", "
+space: .asciiz " "
+
+.text
+.globl main
+
+main:
+    li $t0, 0 #counter
+    la $t1, ints #integers
+
+loop:
+    li $v0, 4
+    la $a0, prompt
+    syscall
+    
+    li $v0, 5
+    syscall
+    move $t2, $v0  # Store input in $t2
+    
+    sw	$t2, 0($t1) #store
+    addi $t0, $t0, 1 #i++
+    add $t1, $t1, 4 #shift 4 for each int
+    blt $t0, 6, loop
+    
+    li $t0, 0 #counter
+    la $t1, ints #integers
+    
+     li $v0, 4
+    la $a0, originalarray
+    syscall
+    
+printarray:
+    lbu	$t2, 0($t1) #load
+    addi $t0, $t0, 1 #i++
+    add $t1, $t1, 4 #shift 4 for each int
+    
+    li $v0, 1
+    move $a0, $t2
+    syscall
+    
+    beq $t0, 6, islastdigit
+    
+    li $v0, 4
+    la $a0, com_space
+    syscall
+islastdigit:
+    li $v0, 4
+    la $a0, space
+    syscall
+
+    blt $t0, 6, printarray
+ 
+ bubblesort:
